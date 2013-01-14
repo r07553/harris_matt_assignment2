e1ece94c6ec11950ce1807f27a991ceb7716a4d3
Matthew Harris <MattHarris@Matthews-MacBook-Pro.local>
2013-01-14 14:12:24 -0500

    Added some variables to my JavaScript.

--- sdi-project2/rename-me.js
+++ sdi-project2/rename-me.js
@@ -2,4 +2,12 @@
 // Matt Harris
 // Date of Completion
 // Project 2
-// Theme Description (1-2 Words) 
+// Video Games
+
+//Start
+
+//My Variables
+var myGames = ["Halo 4", "Black Ops II", "Far Cry 3", "Borderlands 2"]; 
+var minutesPlayed = [20, 35, 120, 15];
+var hoursToPlay= (3);
+var myName = ("Matt Harris");
