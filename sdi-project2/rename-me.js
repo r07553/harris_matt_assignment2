a32a449ec35c3f2d556aa4cb0584d6993357bc2c
Matthew Harris <MattHarris@Matthews-MacBook-Pro.local>
2013-01-14 14:51:10 -0500

    Added a function, might change later.

--- sdi-project2/rename-me.js
+++ sdi-project2/rename-me.js
@@ -8,6 +8,22 @@
 
 //My Variables
 var myGames = ["Halo 4", "Black Ops II", "Far Cry 3", "Borderlands 2"]; 
-var minutesPerGame = [20, 35, 120, 15];
+var minutesPerGame = [24, 36, 120, 16];
 var hoursToPlay= (3);
 var myName = ("Matt Harris");
+var playOneGame = function(){
+	var myGame = myGames[gameNum],
+	minutesThisGame = minutesPerGame[gameNum];
+	console.log("I start playing " + myGame + " for "
+		+ minutesThisGame + " minutes.");
+	for (var minutes = 0; minutes < minutesThisGame; minutes += 10) {
+		var minutesLeft =   minutesThisGame - minutes;
+		console.log(minutes + " minutes played, " + minutesLeft + " minutes until I finish 			    playing.");
+	}
+	console.log("I'm bored with " + myGame + ".");
+};
+for (var gameNum = 0; gameNum < myGames.length; gameNum++) {
+	playOneGame();
+};
+
+console.log("Wow I have played video games for over " + hoursToPlay + " hours. Time to do some homework.");
